bdeba7d8464eff96ddc91f7d8dc8f2f522937f9a
Alex <rocha.alex.oliveira@gmail.com>
2021-06-25 11:40:17 -0300

    first commit

---
+++ run.py
@@ -0,0 +1,6 @@
+import sys
+
+#def main(argv):
+#    if argv[1] == 'master':
+#        from main import run as master
+#        master
---
+++ utils/params.py
@@ -0,0 +1,5 @@
+# PARÂMETROS
+
+# DIRETÓRIOS -----------------
+DICT = 'wrf_out/'
+ARQ_NAME = 'wrfout_20200102.nc'
