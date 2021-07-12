daf4f21bfe84fb89541c2724fc73994d75bce9f5
Alex <rocha.alex.oliveira@gmail.com>
2021-07-12 16:27:24 -0300

    commit final

--- utils/graphics.py
+++ utils/graphics.py
@@ -3,6 +3,7 @@ from matplotlib.cbook import get_sample_data
 import matplotlib.cm as cm
 import numpy as np
 from matplotlib.colors import ListedColormap
+from seaborn.matrix import heatmap
 from wrf.util import _TIME_COORD_VARS
 from utils.winds_function import windsvel
 import seaborn as sns
@@ -12,27 +13,6 @@ def plot(TIME, LAT, LONG, TEMP, PRESS, POT_TEMP, REL_HUM, SPECIF_HUM, U, V, PREC
     # PLOT DA FIGURA GLOBAL ------------------------------
     fig, (ax0, ax1, ax3, ax4) = plt.subplots(4, figsize=(8,10), sharex=True) # MUDAR PARA TRUE
     # ----------------------------------------------------
-
-    # FIGURA DO VENTO ------------------------------------
-    vel_vento = windsvel(U, V)
-    ax0.barbs(TIME, vel_vento, U, V)
-    color = '0'
-    ax0.set_title('\nPrevisão do BRAMS para\nLatitude %.5f, Longitude %.5f e Altura 760 m' %(LAT, LONG))
-    ax0.set_ylabel('Vento (m/s) - Theta (k)')
-    ax0.tick_params(axis='y', labelcolor=color)
-    ax0.grid()    
-
-    #ax0c = ax0.twinx()
-    #sns.heatmap(data=SPECIF_HUM, yticklabels=False, alpha=0.5, ax=ax0c)
-
-    ax0b = ax0.twinx() 
-    color = 'tab:red'    
-    ax0b.plot(TIME, POT_TEMP, '-o', color=color)
-    ax0b.set_ylabel('Theta (K)', color=color)
-    ax0b.tick_params(axis='y', labelcolor=color)
-
-    # ----------------------------------------------------
-
 
     # FIGURA DE TEMPERATURA E PRESSÃO --------------------
     color = '0'
@@ -47,6 +27,29 @@ def plot(TIME, LAT, LONG, TEMP, PRESS, POT_TEMP, REL_HUM, SPECIF_HUM, U, V, PREC
     ax2.plot(TIME, TEMP, '-o', color=color)
     ax2.tick_params(axis='y', labelcolor=color) 
     ax1.grid()
+    # ----------------------------------------------------
+
+    # FIGURA DO VENTO ------------------------------------
+    ax0c = ax0.twinx()
+    #df = pd.DataFrame(zip(SPECIF_HUM[0], SPECIF_HUM[1],SPECIF_HUM[2],SPECIF_HUM[3],SPECIF_HUM[4],SPECIF_HUM[5],SPECIF_HUM[6],SPECIF_HUM[7],SPECIF_HUM[8],SPECIF_HUM[9],SPECIF_HUM[10],SPECIF_HUM[11],SPECIF_HUM[12],SPECIF_HUM[13],SPECIF_HUM[14],SPECIF_HUM[15],SPECIF_HUM[16],SPECIF_HUM[17],SPECIF_HUM[18],SPECIF_HUM[19],SPECIF_HUM[20], SPECIF_HUM[21],SPECIF_HUM[22],SPECIF_HUM[23],SPECIF_HUM[24]) , columns=TIME)
+    #print(df)
+    
+    #sns.heatmap(data=df, xticklabels=TIME, yticklabels=False, alpha=0.5, ax=ax0c, cbar = False)
+    
+    vel_vento = windsvel(U, V)
+    ax0.barbs(TIME, vel_vento, U, V)
+    color = '0'
+    ax0.set_title('\nPrevisão do BRAMS para\nLatitude %.5f, Longitude %.5f e Altura 760 m' %(LAT, LONG))
+    ax0.set_ylabel('Vento (m/s)')
+    ax0.tick_params(axis='y', labelcolor=color)
+    ax0.grid()       
+    
+    ax0b = ax0.twinx() 
+    color = 'tab:red'    
+    ax0b.plot(TIME, POT_TEMP, '-o', color=color)
+    ax0b.set_ylabel('Theta (K)', color=color)
+    ax0b.tick_params(axis='y', labelcolor=color)
+
     # ----------------------------------------------------
 
     # FIGURA DE UMIDADE RELATIVA (%) ---------------------    
